f6a3bffbfe01c208660eebf0ad91ea02e2982891
rizvash <rizvash@dev-sdbox.myguest.virtualbox.org>
2025-07-28 14:42:48 +0300

    feat: update AWS configuration for eu-north-1 region
    - Update AWS region from us-east-1 to eu-north-1
    - Update availability zones to eu-north-1a and eu-north-1b
    - Update aws-credentials.txt with new account information
    - Add AWS console login details for menteebot-observability account
    
    AWS Account Details:
    - Console: https://menteebot-observability.signin.aws.amazon.com/console
    - Username: rizvash.i
    - Region: eu-north-1
    
    Changes:
    - infrastructure/terraform/variables.tf: Update region and AZs
    - aws-credentials.txt: Update region and add account info
    
    Status: ✅ AWS CLI working with new region
    Status: ✅ Terraform validation successful
    Status: ✅ Ready for eu-north-1 deployment

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-north-1"
 }
 
 variable "environment" {
@@ -25,7 +25,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["eu-north-1a", "eu-north-1b"]
 }
 
 variable "ecr_repository_name" {
